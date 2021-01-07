447d2d2585a8ee09b26f70a71ffac3b999b69647
rahul8521 <rahulpradhan98756666@gmail.com>
2021-01-07 09:04:16 +0530

    i added this file

---
+++ function example.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+void message1();
+void message2();
+int main()
+{
+message1() ;
+message2() ;
+return 0 ;
+}
+void message2()
+{
+printf ("But the butter was bitter\n") ;
+}
+void message1()
+{
+printf ("Mary bought some butter\n") ;
+}
