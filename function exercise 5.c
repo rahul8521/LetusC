bb3669814b4174d52ca249db087db2eeeee9dc01
rahul8521 <rahulpradhan98756666@gmail.com>
2021-01-07 09:13:20 +0530

    i added this file

---
+++ function exercise 5.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+int area(int a, int b, int c);
+void main()
+{
+    int a, b, c;
+
+    printf("Enter a: ");
+    scanf("%d", &a);
+    printf("Enter b: ");
+    scanf("%d", &b);
+    printf("Enter c: ");
+    scanf("%d", &c);
+
+    area(a,b,c);
+}
+int area(int a, int b, int c)
+{
+    float S,ar;
+
+    S = (a+b+c)/2;
+
+    ar = sqrt(S*(S-a)*(S-b)*(S-c));
+
+    printf("Area of Triangle: %f",ar);
+}
